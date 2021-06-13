e7c7ce21f6198a889db9f25542bec72dd3b57650
Geraldine Echavarría <geraldinee@talentu.co>
2021-06-12 21:28:01 -0500

    Se adjuntan archivo .py y el requirements

--- arrays/matrices/traspuesta.py
+++ arrays/matrices/traspuesta.py
@@ -13,3 +13,12 @@ print("Original")
 print_r(matriz)
 print("TRANSPUESTA")
 print_r(transpuesta(matriz))
+
+
+def transpuesta(matriz):
+    return [[matriz[j][i] for j in range(len(matriz))] for i in range(len(matriz[0]))]
+mat_1= [[1,2,3],[4,5,6]]
+
+print("Esta es la matriz original",mat_1)
+matriz_resultado=transpuesta(mat_1)
+print("Esta es la transpuesta",matriz_resultado)
