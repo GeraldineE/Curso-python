713969b637494120c9e94c2b8d2e6cd59f251259
Geraldine Echavarría <geraldinee@talentu.co>
2021-06-25 23:20:16 -0500

    interfaces graficas

---
+++ interfaces_graficas/turtle_cuadrado_con_fondo.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+color('red', 'red')
+
+begin_fill()
+
+i=0
+while i<4:
+    fd(100)
+    rt(90)
+    i+=1
+    
+end_fill()
+done()
---
+++ interfaces_graficas/tutle_cuadrado.py
@@ -0,0 +1,28 @@
+import turtle
+
+screen = turtle.Screen()
+turtle = turtle.Turtle()
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
