4f11a67b63e23eab4f24a946cede51bd3383fe8e
Geraldine Echavarría <geraldinee@talentu.co>
2021-06-25 23:20:26 -0500

    archivo de pilas

--- pilas/crear_pila_otra_solucion.py
+++ pilas/crear_pila_otra_solucion.py
@@ -22,3 +22,30 @@ stack.insertar_elemento('Primer elemento')
 print(stack.recorrer())
 stack.insertar_elemento('Segundo elemento')
 print(stack.recorrer())
+
+
+
+
+#################
+
+
+# contruir una pila
+
+class Pila:
+    def __init__(self):
+        self.elemento=[] ##definir el elemento como algo vacio
+    def elemento_vacio(self):
+        return self.elemento == [] ##devolver un boleano
+    def apilar(self,elemento):
+        self.elemento.append(elemento)
+    def remover(self): 
+        self.elemento.pop() 
+    def devolver(self): 
+        print(self.elemento)
+
+    
+pila = Pila()
+pila.elemento_vacio()
+pila.apilar(1)
+pila.apilar(2)
+pila.devolver()
